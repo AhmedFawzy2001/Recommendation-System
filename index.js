ae30d5b96a1b463e72a26327a6e302bd31756576
Ahmed Fawzy <eng.ahmedmohamedfawzy2024@gmail.com>
2024-04-19 15:34:24 +0200

    seventhcommit

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-// const express = require("express")
-// const app = express()
 
-// require('dotenv').config()
-
-// app.use(express.json())
-
-
-// const bookRouter = require('./routes/book.router')
-
-// app.use("/api/v1/books", bookRouter)
-
-// app.listen(process.env.PORT, () => console.log("Server is running on port 5000"))
 
 const express = require('express');
 const bodyParser = require('body-parser');
