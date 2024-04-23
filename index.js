65341df0cd2e9eb274c74efc18e9d6434ee437ff
Ahmed Fawzy <eng.ahmedmohamedfawzy2024@gmail.com>
2024-04-23 23:07:44 +0200

    final

--- index.js
+++ index.js
@@ -128,20 +128,34 @@ app.post('/rating', async (req, res) => {
 //     }
 //   });
 
-app.get('/search', async (req, res) => {
-    const { title } = req.query;
-    try {
+// app.get('/search', async (req, res) => {
+//     const { title } = req.query;
+//     try {
+//       const result = await pool.query(`SELECT * FROM movies WHERE title ILIKE $1`, ['%' + title + '%']);
+//       if (result.rows.length > 0) {
+//         res.json(result.rows); // Return all matching movies
+//       } else {
+//         res.status(404).json({ error: 'Movie not found' });
+//       }
+//     } catch (error) {
+//       console.error('Error executing query', error);
+//       res.status(500).json({ error: 'Internal server error' });
+//     }
+//   });
+app.post('/search', async (req, res) => {
+  const { title } = req.body; // Extract the title from the request body
+  try {
       const result = await pool.query(`SELECT * FROM movies WHERE title ILIKE $1`, ['%' + title + '%']);
       if (result.rows.length > 0) {
-        res.json(result.rows); // Return all matching movies
+          res.json(result.rows); // Return all matching movies
       } else {
-        res.status(404).json({ error: 'Movie not found' });
+          res.status(404).json({ error: 'Movie not found' });
       }
-    } catch (error) {
+  } catch (error) {
       console.error('Error executing query', error);
       res.status(500).json({ error: 'Internal server error' });
-    }
-  });
+  }
+});
 
   
 
