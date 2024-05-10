56b4a1b32b9a15b1022953825eda6615905643f9
Ahmed Fawzy <eng.ahmedmohamedfawzy2024@gmail.com>
2024-05-10 16:49:09 +0300

    clean project Structure

--- database.js
+++ database.js
@@ -4,15 +4,19 @@ require('dotenv').config(); // Load environment variables from .env file
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    user: 'vercel_db_35rb_user',
+    host: 'dpg-coku3qud3nmc739lls40-a.oregon-postgres.render.com',
+    database: 'vercel_db_35rb',
+    password: 'dSYKqdUoLtuKhljWHsE4I0lcl29UxIni',
+    port: 5432, // Default PostgreSQL port
     ssl: {
+        // You may need to provide SSL certificate options here
+        // Consult your PostgreSQL server documentation for details
         rejectUnauthorized: false // For development purposes, may need to be configured properly in production
-    }
-});
+      }
+  });
+  
+
 
 module.exports = pool;
   // Connect to the PostgreSQL database
@@ -43,7 +47,16 @@ module.exports = pool;
 //         rejectUnauthorized: false // For development purposes, may need to be configured properly in production
 //       }
 //   });
-  
+//   const pool = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_PORT,
+//     ssl: {
+//         rejectUnauthorized: false // For development purposes, may need to be configured properly in production
+//     }
+// });
 
  // const pool = new Pool({
 //   connectionString: process.env.POSTGRES_URL + "?sslmode=require",
