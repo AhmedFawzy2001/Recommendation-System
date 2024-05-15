8926b1c31438351eb558de1e4ee05cc94db3bd34
the7ag <mohamed.dead1543@gmail.com>
2024-05-16 01:36:47 +0300

    Limited the search

--- controller/searchController.js
+++ controller/searchController.js
@@ -3,7 +3,7 @@ const pool = require("../database"); // Assuming database.js is in the parent di
 const searchMovies = async (req, res) => {
     const { title } = req.body; // Extract the title from the request body
     try {
-        const result = await pool.query(`SELECT * FROM movies WHERE title ILIKE $1`, ['%' + title + '%']);
+        const result = await pool.query(`SELECT * FROM movies WHERE title ILIKE $1 LIMIT 20`, ['%' + title + '%']);
         if (result.rows.length > 0) {
             res.json(result.rows); // Return all matching movies
         } else {
