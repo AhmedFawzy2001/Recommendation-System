4c1a37d24683902850d740b5d519fe727bd05e45
Ahmed Fawzy <eng.ahmedmohamedfawzy2024@gmail.com>
2024-05-16 01:39:58 +0300

    final

--- controller/searchController.js
+++ controller/searchController.js
@@ -3,9 +3,9 @@ const pool = require("../database"); // Assuming database.js is in the parent di
 const searchMovies = async (req, res) => {
     const { title } = req.body; // Extract the title from the request body
     try {
-        const result = await pool.query(`SELECT * FROM movies WHERE title ILIKE $1`, ['%' + title + '%']);
+        const result = await pool.query(`SELECT * FROM movies WHERE title ILIKE $1 LIMIT 20`, ['%' + title + '%']);
         if (result.rows.length > 0) {
-            res.json(result.rows); // Return all matching movies
+            res.json(result.rows); // Return the first 20 matching movies
         } else {
             res.status(404).json({ error: 'Movie not found' });
         }
