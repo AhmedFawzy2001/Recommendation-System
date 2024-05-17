a8de32f8445dd39c4c48ee1cad56a680382ba864
Ahmed Fawzy <eng.ahmedmohamedfawzy2024@gmail.com>
2024-05-17 15:43:29 +0300

    final

--- controller/ratingController.js
+++ controller/ratingController.js
@@ -1,43 +1,73 @@
-
 const pool = require("../database"); // Assuming database.js is in the parent directory
 const { generateTimestamp } = require("../controller/userController");
 
 const addRating = async (req, res) => {
-    const { userid, movieid, rating } = req.body;
-    const parsedRating = Math.round(parseFloat(rating)); // Parse to float, round to nearest integer, and then convert to integer
-    const timestamp = generateTimestamp();
-    try {
-        // Check if the user has already rated the movie
-        const existingRatingQuery = 'SELECT * FROM ratings WHERE userid = $1 AND movieid = $2';
-        const existingRatingResult = await pool.query(existingRatingQuery, [userid, movieid]);
+  const { userid, movieid, rating } = req.body;
+  const parsedRating = Math.round(parseFloat(rating)); // Parse to float, round to nearest integer, and then convert to integer
+  const timestamp = generateTimestamp();
+  try {
+    const updateFlagQuery = {
+      text: "UPDATE users SET flag = false WHERE userid = $1 RETURNING *",
+      values: [ID],
+    };
+    await pool.query(updateFlagQuery);
+    // Check if the user has already rated the movie
+    const existingRatingQuery =
+      "SELECT * FROM ratings WHERE userid = $1 AND movieid = $2";
+    const existingRatingResult = await pool.query(existingRatingQuery, [
+      userid,
+      movieid,
+    ]);
 
-        if (existingRatingResult.rows.length > 0) {
-            // If user has already rated the movie, update the existing rating
-            const updateRatingQuery = 'UPDATE ratings SET rating = $1, timestamp = $2 WHERE userid = $3 AND movieid = $4 RETURNING userid';
-            const updateRatingResult = await pool.query(updateRatingQuery, [parseInt(parsedRating), timestamp, userid, movieid]);
-            
-            if (updateRatingResult.rows.length > 0) {
-                res.status(200).send({ message: 'Rating Updated Successfully', Userid: updateRatingResult.rows[0].userid });
-            } else {
-                res.status(500).send({ message: 'Failed to update rating' });
-            }
-        } else {
-            // If user has not rated the movie before, insert a new rating
-            const insertRatingQuery = 'INSERT INTO ratings (userid, movieid, rating, timestamp) VALUES ($1, $2, $3, $4) RETURNING userid';
-            const insertRatingResult = await pool.query(insertRatingQuery, [userid, movieid, parseInt(parsedRating), timestamp]);
+    if (existingRatingResult.rows.length > 0) {
+      // If user has already rated the movie, update the existing rating
+      const updateRatingQuery =
+        "UPDATE ratings SET rating = $1, timestamp = $2 WHERE userid = $3 AND movieid = $4 RETURNING userid";
+      const updateRatingResult = await pool.query(updateRatingQuery, [
+        parseInt(parsedRating),
+        timestamp,
+        userid,
+        movieid,
+      ]);
 
-            if (insertRatingResult.rows.length > 0) {
-                res.status(200).send({ message: 'Rating Added Successfully', Userid: insertRatingResult.rows[0].userid });
-            } else {
-                res.status(500).send({ message: 'Failed to add rating' });
-            }
-        }
-    } catch (err) {
-        console.error('Error during rating:', err);
-        res.status(500).send('An error occurred during rating');
+      if (updateRatingResult.rows.length > 0) {
+        res
+          .status(200)
+          .send({
+            message: "Rating Updated Successfully",
+            Userid: updateRatingResult.rows[0].userid,
+          });
+      } else {
+        res.status(500).send({ message: "Failed to update rating" });
+      }
+    } else {
+      // If user has not rated the movie before, insert a new rating
+      const insertRatingQuery =
+        "INSERT INTO ratings (userid, movieid, rating, timestamp) VALUES ($1, $2, $3, $4) RETURNING userid";
+      const insertRatingResult = await pool.query(insertRatingQuery, [
+        userid,
+        movieid,
+        parseInt(parsedRating),
+        timestamp,
+      ]);
+
+      if (insertRatingResult.rows.length > 0) {
+        res
+          .status(200)
+          .send({
+            message: "Rating Added Successfully",
+            Userid: insertRatingResult.rows[0].userid,
+          });
+      } else {
+        res.status(500).send({ message: "Failed to add rating" });
+      }
     }
+  } catch (err) {
+    console.error("Error during rating:", err);
+    res.status(500).send("An error occurred during rating");
+  }
 };
 
 module.exports = {
-    addRating
+  addRating,
 };
--- controller/recommendController.js
+++ controller/recommendController.js
@@ -1,33 +1,72 @@
-
-
-const axios = require('axios');
+const axios = require("axios");
 
 const pool = require("../database"); // Assuming database.js is in the parent directory
 
 const recommendMovies = async (req, res) => {
-    try {
-        const flutterData = req.body;
-
-        // Send data to Python script
-        const pythonResponse = await axios.post('https://movie-recommendation-api-jyof.onrender.com/process-data', flutterData);
-        const ids = pythonResponse.data.recommendations;
-        console.log(ids);
-
-        // Fetch movies with the retrieved IDs from the database
-        const query = {
-            text: 'SELECT * FROM movies WHERE movieid = ANY($1)',
-            values: [ids],
-        };
-        const result = await pool.query(query);
-
-        // Send the fetched movies back to Flutter
-        res.json(result.rows);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error processing data');
+  try {
+    const flutterData = req.body;
+    const ID = req.id;
+    const userQuery = {
+      text: "SELECT flag FROM users WHERE userid = $1",
+      values: [ID],
+    };
+    const userResult = await pool.query(userQuery);
+    if (userResult.rowCount === 0) {
+      return res.status(404).send("User not found");
     }
+    const userFlag = userResult.rows[0].flag;
+    if (userFlag) {
+      const userRecommendationQuery = {
+        text: "SELECT recommendations FROM users WHERE userid = $1",
+        values: [ID],
+      };
+      const userRecommendationResult = await pool.query(
+        userRecommendationQuery
+      );
+      res.json(userRecommendationResult.rows);
+    } else {
+      // Send data to Python script
+      const pythonResponse = await axios.post(
+        "https://movie-recommendation-api-jyof.onrender.com/process-data",
+        flutterData
+      );
+      const ids = pythonResponse.data.recommendations;
+      console.log(ids);
+      // Fetch movies with the retrieved IDs from the database
+      const query = {
+        text: "SELECT * FROM movies WHERE movieid = ANY($1)",
+        values: [ids],
+      };
+      const result = await pool.query(query);
+      const defaultPosterUrl =
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEUac6GqZPOX113dy-FfGX6NrDLsp1qEvN1LrkX-GewqK1-Kz8J_PTM2y6&s=10";
+      const defaultGenres = "{{Drama}}";
+      const defaultCast = "{{hady}}";
+      result.rows.forEach((movie) => {
+        if (!movie.poster) {
+          movie.poster = defaultPosterUrl;
+        }
+        if (!movie.cast) {
+          movie.cast = defaultCast;
+        }
+        if (!movie.genres) {
+          movie.genres = defaultGenres;
+        }
+      });
+      const updateFlagtrue = {
+        text: "UPDATE users SET flag = true WHERE userid = $1 RETURNING *",
+        values: [ID],
+      };
+      await pool.query(updateFlagtrue);
+      // Send the fetched movies back to Flutter
+      res.json(result.rows);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error processing data");
+  }
 };
 
 module.exports = {
-    recommendMovies
+  recommendMovies,
 };
